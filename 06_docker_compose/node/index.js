99759587ef319e5e41b81a78b154013d117b5690
Rogerio Pereira <hi@rogeriopereira.dev>
2023-05-15 19:38:46 -0400

    Node saving data in database

--- 06_docker_compose/node/index.js
+++ 06_docker_compose/node/index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) VALUES('Rogerio')`
+connection.query(sql)
+connection.end()
+
 app.get('/', (req, res) => {
     res.send('<h1>Node - Full Cycle</h1>')
 })
